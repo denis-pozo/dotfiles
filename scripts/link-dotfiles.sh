19b18b24f9fe4b669a81458cc15ba911b99f5c69
Denis Pozo <denispzpd@gmail.com>
2020-03-15 17:57:33 +0100

    Move bootstraping script to root

--- scripts/link-dotfiles.sh
+++ scripts/link-dotfiles.sh
@@ -1,23 +1,4 @@
 #!/usr/bin/env bash
-set -e
+set -ex
 
-DOTFILES="$PWD/../home"
-
-link () {
-        echo "This utility will symlink the files in this repo to the home directory"
-        echo "Proceed? (y/n)"
-        read resp
-        if [ "$resp" = 'y' -o "$resp" = 'Y' ] ; then
-                cd "$DOTFILES"
-                for file in $( ls -A | grep -vE '\.exclude*|\.git$|\.gitignore|.*.md' ) ; do
-                        ln -sfv "$DOTFLIES/$file" "$HOME"
-                done
-                echo "Symlinking complete"
-                cd ..
-        else
-                echo "Symlinking cancelled by user"
-                return 1
-        fi
-}
-
-link
+stow home
